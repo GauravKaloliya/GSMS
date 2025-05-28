6ca1340bed10b25664e20b956895c76a9c7d22d5
Gaurav Kaloliya <iamgaurav225@gmail.com>
2025-05-28 17:47:04 +0000

    modified:   API/routes/user/user.route.js

--- API/routes/user/user.route.js
+++ API/routes/user/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { savePushToken } = require('../user/user.controller');
+const { savePushToken } = require('../../controllers/user/user.controller');
 
 router
   .post('/push-token', savePushToken);
