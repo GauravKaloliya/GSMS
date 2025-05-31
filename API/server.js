895ff304265a0f4bb76bdf2d058d7239a02b3526
Gaurav Kaloliya <iamgaurav225@gmail.com>
2025-05-31 09:10:40 +0000

    modified:   .gitignore 	deleted:    API/client/.gitignore 	deleted:    API/client/dist/assets/index-ConeeV4V.js 	deleted:    API/client/dist/assets/index-D8b4DHJx.css 	deleted:    API/client/dist/index.html 	deleted:    API/client/eslint.config.js 	deleted:    API/client/index.html 	deleted:    API/client/package-lock.json 	deleted:    API/client/package.json 	deleted:    API/client/src/App.css 	deleted:    API/client/src/App.jsx 	deleted:    API/client/src/index.css 	deleted:    API/client/src/main.jsx 	deleted:    API/client/vite.config.js 	modified:   API/server.js

--- API/server.js
+++ API/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 const { captureResponseBody, logApiRequest } = require('./routes/utils/apiRequest');
 const middleware = require('./routes/utils/middleware');
@@ -18,15 +17,6 @@ app.use(logApiRequest);
 app.use('/api', authRoutes);
 app.use(middleware);
 app.use('/api/user', userRoutes);
-
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-app.get('/*', (req, res) => {
-  if (req.path.startsWith('/api')) {
-    return res.status(404).json({ error: 'Not found' });
-  }
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 app.use((req, res) => res.status(404).json({ error: 'Not found' }));
 
