5b7a8ea36c36b16b56c4b33682bc37c8ac841566
Gaurav Kaloliya <iamgaurav225@gmail.com>
2025-05-31 08:30:29 +0000

    modified:   API/server.js

--- API/server.js
+++ API/server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html for root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 // Serve login.html for /login route
